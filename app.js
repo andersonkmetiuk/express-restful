60feab6601ee04f58935ca63a09a8be0cece2d19
Anderson Kmetiuk <58405194+andersonkmetiuk@users.noreply.github.com>
2024-03-21 01:15:23 -0300

    v7.0.0 MongooseError: Model.find() no longer accepts a callback

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+const db = mongoose.connect("mongodb://localhost/bookAPI");
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000; // port or default to 3000
+const Book = require("./models/bookModel");
 
-bookRouter.route('/books')
+bookRouter.route("/books")
   .get((req, res) => {
-    const response = { hello: 'This is my API'};
-    res.json(response);
+    Book.find((err, book) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(book);
   });
-app.use('/api', bookRouter);
+});
+app.use("/api", bookRouter);
 
 // every time there's a get request '/' we are going to respond with a function containing 'request' and 'response' (req, res)=>{}
-app.get('/', (req, res) => {
-  res.send('Welcome to my API');
+app.get("/", (req, res) => {
+  res.send("Welcome to my API");
 });
 
 //let's listen to the PORT
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+  console.log(`Running on http://localhost:${port}/api/books`);
 });
